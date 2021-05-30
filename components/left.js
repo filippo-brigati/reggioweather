8a09fb5672275f0b8a4bdde8b68b6e40bef61062
Filippo Brigati <filippobrigati2@gmail.com>
2021-05-30 22:35:25 +0200

    dark theme 🌚🌝

--- components/left.js
+++ components/left.js
@@ -1,16 +1,24 @@
-import Image from 'next/image'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; // Import the FontAwesomeIcon component
+import { faCloud, faCloudShowersHeavy, faCloudSun, faCloudSunRain, faSun, faSnowflake, faPooStorm, faSmog } from "@fortawesome/free-solid-svg-icons";
 
 const Left = (data) => {
+    let iconPath = null
+
+    if (data.props.icon_id === 800) { iconPath = faSun }
+    else if(data.props.icon_id > 802) { iconPath = faCloud }
+    else if(data.props.icon_id === 801) { iconPath = faCloudSun }
+    else if(data.props.icon_id >= 701 && data.props.icon_id <= 781) { iconPath = faSmog }
+    else if(data.props.icon_id >= 600 && data.props.icon_id <= 622) { iconPath = faSnowflake }
+    else if(data.props.icon_id >= 500 && data.props.icon_id <= 531) { iconPath = faCloudSunRain }
+    else if(data.props.icon_id >= 300 && data.props.icon_id <= 321) { iconPath = faCloudShowersHeavy }
+    else if(data.props.icon_id >= 200 && data.props.icon_id <= 232) { iconPath = faPooStorm }
+    else { iconPath += 'error.svg' }
+
     return (
-        <div>
+        <div className="bg-white dark:bg-gray-700">
             <h3 className="font-semibold text-2xl flex items-center justify-center">{data.props.name}, {data.props.state}</h3>
             <div className="flex justify-center items-center py-6">
-                <Image
-                    src={data.props.iconPATH}
-                    alt="Picture of the author"
-                    width={50}
-                    height={50}
-                />
+                <FontAwesomeIcon icon={iconPath} className="text-black dark:text-gray-100" size="4x"></FontAwesomeIcon>
             </div>
             <h3 className="font-semibold text-2xl flex items-center justify-center">{data.props.condition}</h3>
         </div>
--- pages/index.js
+++ pages/index.js
@@ -18,29 +18,14 @@ export async function getStaticProps(context) {
 }
 
 export default function Home({ data }) {
-  console.log(data)
-  let iconPath = '/img/'
-
-  if (data.weather[0].id === 800) { iconPath += 'sun-solid.svg' }
-  else if(data.weather[0].id > 802) { iconPath += 'cloud-solid.svg' }
-  else if(data.weather[0].id === 801) { iconPath += 'cloud-sun-solid.svg' }
-  else if(data.weather[0].id >= 701 && data.weather[0].id <= 781) { iconPath += 'smog-solid.svg' }
-  else if(data.weather[0].id >= 600 && data.weather[0].id <= 622) { iconPath += 'snowflake-solid.svg' }
-  else if(data.weather[0].id >= 500 && data.weather[0].id <= 531) { iconPath += 'cloud-sun-rain-solid.svg' }
-  else if(data.weather[0].id >= 300 && data.weather[0].id <= 321) { iconPath += 'cloud-showers-heavy-solid.svg' }
-  else if(data.weather[0].id >= 200 && data.weather[0].id <= 232) { iconPath += 'poo-storm-solid.svg' }
-  else { iconPath += 'error.svg' }
-
-  //console.log(iconPath)
-
   return (
     <>
-        <div className="min-w-screen min-h-screen flex items-center justify-center px-5 py-5 bg-white dark:bg-gray-800">
+        <div className="min-w-screen min-h-screen flex items-center justify-center px-5 py-5 bg-white dark:bg-gray-600">
             <div className="rounded shadow-xl overflow-hidden w-full md:flex" style={{ maxWidth: 900 }} x-init="stockTicker.renderChart()">
-                <div className="flex w-full md:w-1/3 p-10 bg-white text-gray-600 items-center justify-center">
-                    <LeftComponent props={{ name: data.name, state: data.sys.country, condition: data.weather[0].main, iconPATH: iconPath }} />
+                <div className="flex w-full md:w-1/3 p-10 bg-white dark:bg-gray-700 dark:text-gray-50 text-gray-600 items-center justify-center">
+                    <LeftComponent props={{ name: data.name, state: data.sys.country, condition: data.weather[0].main, icon_id: data.weather[0].id }} />
                 </div>
-                <div className="flex w-full md:w-2/3 p-10 bg-gray-100 text-gray-600 justify-center items-center">
+                <div className="flex w-full md:w-2/3 p-10 bg-gray-100 dark:bg-gray-600 text-gray-600 dark:text-gray-50 justify-center items-center">
                     <RightComponet props={{ temp: data.main.temp.toFixed(1), feelTemp: data.main.feels_like.toFixed(1), t_max: data.main.temp_max.toFixed(1), t_min: data.main.temp_min.toFixed(1), hum: data.main.humidity.toFixed(1) }}/>
                 </div>
             </div>
